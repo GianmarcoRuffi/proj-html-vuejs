dc036a92cf2c99d77b41740a419474644a0546c0
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-16 19:06:31 +0200

    Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,35 @@
 <template>
   <section id="jumbotron">
     <div class="container hero h-100 py-4">
-      <div class="row h-100">
-        <div class="column h-100 d-flex flex-column py-5 justify-content-end">
-          <div class="row py-3">
-            <div class="col-7 col-md-8 col-lg-3">
-              <img
-                src="../assets/img/avadabarbers_hero_focalmirror.png"
-                alt="barber"
-                class="img-fluid"
-              />
-            </div>
-          </div>
+      <div class="row d-flex justify-content-between">
+        <div class="col-7 col-md-8 col-lg-3">
+          <img
+            src="../assets/img/avadabarbers-logo-x1.png"
+            alt="barber"
+            class="img-fluid"
+          />
+        </div>
+        <div class="col-7 col-md-8 col-lg-3 menu">Menu</div>
+      </div>
+      <!-- *********** -->
+      <div
+        class="row h-100 py-3 d-flex justify-content-between align-items-center"
+      >
+        <div class="col-12 col-lg-6">
+          <h1 class="display-5 py-2">Barber Shop</h1>
+          <hr class="ochre" />
+          <p class="lead py-2">The pinnacle of male grooming</p>
+          <p class="lead">
+            <a class="my-btn" href="#" role="button">Learn more</a>
+          </p>
+        </div>
 
-          <div class="row">
-            <div class="col-12 col-lg-4">
-              <h1 class="display-5 py-2">Barber Shop</h1>
-              <hr class="ochre" />
-              <p class="lead py-2">The pinnacle of male grooming</p>
-
-              <p class="lead">
-                <a class="my-btn" href="#" role="button">Learn more</a>
-              </p>
-            </div>
-          </div>
+        <div class="col-7 col-md-8 col-lg-6 d-flex">
+          <img
+            src="../assets/img/avadabarbers_hero_focalmirror.png"
+            alt="barber"
+            class="img-fluid"
+          />
         </div>
       </div>
     </div>
@@ -37,12 +43,16 @@ export default { name: "Header" };
 <style scoped lang="scss">
 @import "../styles/general.scss";
 #jumbotron {
-  height: 930px;
+  height: 980px;
   position: relative;
   color: white;
   background-position: center;
   background-size: cover;
   background-image: url("../assets/img/avadabarbers-homepage-hero-bg.jpg");
+}
+
+.display-5 {
+  font-size: 80px;
 }
 
 .hero {
