90b44cf20f48e0411dca3a3aed2879572d2d2050
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-17 10:33:35 +0200

    Update 2

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -13,6 +13,21 @@
         </div>
       </div>
     </div>
+
+    <div class="triangle">
+      <svg
+        class="fusion-big-triangle-candy"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        width="100%"
+        height="100"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+        style="fill: #ffff; padding: 0"
+      >
+        <path d="M0 100 L50 2 L100 100 Z"></path>
+      </svg>
+    </div>
   </section>
 </template>
 
@@ -30,6 +45,15 @@ export default { name: "MainContent" };
   background-position: top;
   background-size: cover;
   background-image: url("../assets/img/avadabarbers-about-introbackground.jpg");
+}
+
+.triangle {
+  // display: flex;
+  // justify-content: center;
+  position: absolute;
+  bottom: 0;
+  width: 100vw;
+  // transform: rotate(180deg);
 }
 
 ul {
