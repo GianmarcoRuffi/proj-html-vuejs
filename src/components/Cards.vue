a67427527dc8af5092aba66a150da5b68d636f75
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-17 13:07:58 +0200

    cards setup

---
+++ src/components/Cards.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "Cards",
+
+  props: {
+    img: Image,
+    title: String,
+    text: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "./src/styles/vars.scss";
+
+.ochre {
+  color: $ochre;
+}
+</style>
