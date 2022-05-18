42e6d568999572bf2f6db7aae48027390de7d4f3
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-18 15:37:59 +0200

    Featured

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -3,6 +3,30 @@
     <div
       class="container h-100 d-flex flex-column justify-content-end align-items-center"
     >
+      <div class="featured container">
+        <div class="row d-flex">
+          <div class="col-4">
+            <img
+              :src="require(`../assets/img/shaving_butter-800x800.png`)"
+              alt="service"
+              class="img-fluid"
+            />
+          </div>
+          <div class="col-8">
+            <titles
+              text1="This months featured product"
+              text2="Shaving butter"
+            />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque laoreet enim est, vel molestie dolor ultrices ut.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <my-button text="Buy now" />
+          </div>
+        </div>
+      </div>
+
       <div class="row">
         <titles text1="What people say" text2="Reviews" />
       </div>
@@ -30,8 +54,9 @@
 <script>
 import Titles from "./Titles.vue";
 import CardReviews from "./CardReviews.vue";
+import myButton from "./myButton.vue";
 export default {
-  components: { Titles, CardReviews },
+  components: { Titles, CardReviews, myButton },
   name: "Services",
 };
 </script>
@@ -42,6 +67,20 @@ export default {
 #reviews {
   height: 1200px;
   position: relative;
+}
+
+.featured {
+  background-color: #121413;
+  max-height: 40%;
+  position: absolute;
+  top: -60px;
+  max-width: 70%;
+
+  .col-4 {
+    background-image: url("../assets/img/avadabarbers-cta-background.jpg");
+    background-position: top;
+    background-size: cover;
+  }
 }
 
 .triangle {
